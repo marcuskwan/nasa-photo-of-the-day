667c22fe9c52c01e729a60c9b5c33b0fb402445f
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-11 07:22:10 +1000

    thanks vlad jamie and greg, got the image showing!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=i6kXrRAugKWMaP1p9kYk4nKRGQSmCIgqenF7r0hT&date=2012-03-14",
+      )
+      .then(response => setData(response.data.url))
+      .catch(error => "oh no, something went wrong!");
+    // return () => {
+    //   effect
+    // };
+  }, []);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="apod">
+        <img src={data} alt="apod"/>
+      </div>
     </div>
   );
 }
